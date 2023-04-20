7e3c841eebbc9bff65cbf648f5ed10d4e9dcfb98
ThijsMoerland <Moerland8@gmail.com>
2023-04-20 13:28:26 +0200

    update user

--- Back-end/code-generation/src/main/java/SOT/Squad/code/generation/Controllers/UserRestController.java
+++ Back-end/code-generation/src/main/java/SOT/Squad/code/generation/Controllers/UserRestController.java
@@ -4,10 +4,7 @@ import SOT.Squad.code.generation.Models.Users;
 import SOT.Squad.code.generation.Services.UserService;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,11 @@ public class UserRestController {
     @PostMapping("/restusers")
     public Users addUser(@RequestBody Users user) {
         return userService.addUser(user);
+    }
+
+    @PutMapping("/restusers")
+    public Users updateUser(@RequestBody Users user) {
+        return userService.updateUser(user);
     }
 
 }
--- Back-end/code-generation/src/main/java/SOT/Squad/code/generation/Services/UserService.java
+++ Back-end/code-generation/src/main/java/SOT/Squad/code/generation/Services/UserService.java
@@ -20,9 +20,19 @@ public class UserService {
         return user;
     }
 
+    public Users updateUser(Users user) {
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getId() == user.getId()) {
+                users.set(i, user);
+                return user;
+            }
+        }
+        return null;
+    }
+
     public UserService() {
         users.add(new Users(1, "Thijs", "Moerland",681340798,"680942@student.inholland.nl","Söderblomstraat",53,"2131GB","Hoofddorp"));
-        users.add(new Users(1, "Serena", "Kenter",681340798,"680942@student.inholland.nl","Söderblomstraat",53,"2131GB","Hoofddorp"));
-        users.add(new Users(1, "Omar", "Al Sayasna",681340798,"680942@student.inholland.nl","Söderblomstraat",53,"2131GB","Hoofddorp"));
+        users.add(new Users(2, "Serena", "Kenter",681340798,"680942@student.inholland.nl","Söderblomstraat",53,"2131GB","Hoofddorp"));
+        users.add(new Users(3, "Omar", "Al Sayasna",681340798,"680942@student.inholland.nl","Söderblomstraat",53,"2131GB","Hoofddorp"));
     }
 }
