b7667eaf2a6847d9a2d75653df11a0da97c9d4f7
647825 <89908526+647825@users.noreply.github.com>
2023-06-07 13:45:56 +0200

    .

--- Front-end/src/components/customers/AccountOverview.vue
+++ Front-end/src/components/customers/AccountOverview.vue
@@ -3,17 +3,16 @@
     <headerNavigation />
 
     <div class="container">
-      <h2>{{ user.firstName }} {{ user.lastName }}</h2>
+      <h2>Personal Info</h2>
       <button @click="goToUserInfo">User Info</button>
-      
-      <div v-if="user.bankAccountList.length > 0">
+            <div v-if="user.bankAccountList.length > 0">
         <h2>IBAN: {{ user.bankAccountList[0].iban }}</h2>
       </div>
 
       <h3>Total: Є{{ totalAmount }}</h3>
       <h3>Bank Accounts</h3>
       <div v-for="account in user.bankAccountList" :key="account.id" @click="goToTransactions(account)">
-        <span class="wide-field">{{ account.accountType }} Є{{ account.amount }}</span>
+        <span class="wide-field">{{ account.id }} {{ account.accountType }} {{ account.amount }}</span>
       </div>
     </div>
 
@@ -45,76 +44,27 @@ export default {
         houseNumber: "",
         postalCode: "",
         city: "",
-        bankAccountList:
-        {
-          id: 0,
-          iban: "",
-          balance: 0,
-          userId: 0,
-          disabled: false,
-          currencies: [],
-          accountType: [],
-          absoluutLimit: 0,
-        }
-        ,
+        bankAccountList: [],
       },
-      bankacc: {
-        id: 0,
-        iban: "",
-        balance: 0,
-        userId: 0,
-        disabled: false,
-        currencies: [],
-        accountType: [],
-        absoluutLimit: 0,
-      }
-
     };
   },
-  computed: {
-    totalAmount() {
-      // return this.user.bankAccountList.reduce((total, account) => total + account.amount, 0);
-    }
-  },
   mounted() {
-    console.log(this.user.bankAccountList);
     this.getAll();
   },
   methods: {
     getAll() {
       axios
-        .get('/users/current', {
+        .get('users/current', {
           headers: {
             Authorization: "Bearer " + localStorage.getItem("jwt")
           }
         })
         .then((res) => {
           this.user = res.data;
-          console.log(this.user.bankAccountList);
-          this.getBankAccounts();
+          console.log(res.data)
+          console.log(this.user.id)
         })
         .catch(error => console.log(error))
-    },
-    getBankAccounts() {
-      // Iterate over the bank accounts and fetch their details
-      for (let i = 0; i < this.user.bankAccountList.length; i++) {
-        const accountId = this.user.bankAccountList[i];
-        axios
-          .get(`/bankaccounts/` + accountId, {
-            headers: {
-              Authorization: "Bearer " + localStorage.getItem("jwt")
-            }
-          })
-          .then((res) => {
-            this.user.bankAccountList[i] = res.data;
-            this.user.bankAccountList[i].amount = res.data.balance;
-            this.user.bankAccountList[i].iban = res.data.iban;
-            this.user.bankAccountList[i].accountType = res.data.accountType;
-          })
-          .catch(error => console.log(error));
-      }
-      this.bankacc = this.user.bankAccountList[1];
-      console.log(this.bankacc);
     },
     goToUserInfo() {
       this.$router.push('/personal-details');
