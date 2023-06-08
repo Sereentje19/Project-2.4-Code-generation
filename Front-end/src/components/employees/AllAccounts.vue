d22a31f329b31ce538f4ec47d8f7925bdc8f6532
647825 <89908526+647825@users.noreply.github.com>
2023-06-08 15:25:18 +0200

    Update AllAccounts.vue

--- Front-end/src/components/employees/AllAccounts.vue
+++ Front-end/src/components/employees/AllAccounts.vue
@@ -1,119 +1,33 @@
 <template>
     <headerNavigation />
-  
+
     <div class="container">
-      <h2>All Users</h2>
-      <div id="inputAndBtn">
-        <input id="inputfield" type="text" v-model="searchQuery" placeholder="Search" />
-        <button id="buttonAdd" class="add-user-button" @click="goToAddUser">Add User</button>
-      </div>
-  
-      <div v-if="!showUsersWithNoAccounts">
-        <button class="filter-button" @click="filterUsersWithNoAccounts">No Accounts</button>
-      </div>
-  
-      <div v-else>
-        <button class="filter-button" @click="resetFilter">Show All</button>
-      </div>
-  
-      <div v-for="account in filteredUsers" :key="account.id" @click="selectUser(account)">
-        <span>{{ account.firstName }} {{ account.lastName }}</span>
-      </div>
+        <h2>All Users</h2>
+        <div id="inputAndBtn">
+            <input id="inputfield" type="text" v-model="searchQuery" placeholder="Search" />
+            <button id="buttonAdd" class="add-user-button" @click="goToAddUser">Add User</button>
+        </div>
+
+        <div v-if="!showUsersWithNoAccounts">
+            <button class="filter-button" @click="filterUsersWithNoAccounts">No Accounts</button>
+        </div>
+
+        <div v-else>
+            <button class="filter-button" @click="resetFilter">Show All</button>
+        </div>
+
+        <div v-for="account in filteredUsers" :key="account.id" @click="selectUser(account)">
+            <span>{{ account.firstName }} {{ account.lastName }}</span>
+        </div>
     </div>
-  
+
     <footerNavigation />
-  </template>
-  <script>
-  import headerNavigation from '../main/Header.vue'
-  import footerNavigation from '../main/Footer.vue';
-  import axios from '../../axios-auth.js';
-  
-  export default {
-    components: {
-      headerNavigation,
-      footerNavigation
-    },
-    data() {
-      return {
-        users: [],
-        selectedUser: null,
-        searchQuery: '',
-        showUsersWithNoAccounts: false
-      };
-    },
-    mounted() {
-      this.getUsers();
-    },
-    computed: {
-      filteredUsers() {
-        if (this.showUsersWithNoAccounts) {
-          return this.users.filter(user => !user.bankAccountList || user.bankAccountList.length === 0);
-        }
-  
-        if (this.searchQuery) {
-          const query = this.searchQuery.toLowerCase();
-          return this.users.filter(user => {
-            const fullName = `${user.firstName} ${user.lastName}`.toLowerCase();
-            return fullName.includes(query);
-          });
-        }
-  
-        return this.users;
-      }
-    },
-    methods: {
-      getUsers() {
-        axios
-          .get('/users', {
-            headers: {
-              Authorization: "Bearer " + localStorage.getItem("jwt")
-            }
-          })
-          .then((res) => {
-            this.users = res.data;
-          })
-          .catch(error => console.log(error));
-      },
-      goToAddUser() {
-        this.$router.push('/AddUser');
-      },
-      selectUser(user) {
-        this.$router.push(`/employee/accounts/` + btoa(user.id));
-      },
-      filterUsersWithNoAccounts() {
-        this.showUsersWithNoAccounts = true;
-      },
-      resetFilter() {
-        this.showUsersWithNoAccounts = false;
-      }
-    }
-  };
-  </script>
-  
+</template>
+<script>
+import headerNavigation from '../main/Header.vue'
+import footerNavigation from '../main/Footer.vue';
+import axios from '../../axios-auth.js';
 
-  
-<style>
-#inputAndBtn {
-    display: flex;
-    justify-content: space-between;
-    color: none;
-}
-
-#buttonAdd {
-    height: 40px;
-    margin-bottom: 10px;
-}
-
-#inputfield {
-    height: 40px;
-}
-
-<<<<<<< HEAD
-#buttonFilter {
-    height: 40px;
-    margin-left: 10px;
-}
-=======
 export default {
     components: {
         headerNavigation,
@@ -123,7 +37,8 @@ export default {
         return {
             users: [],
             selectedUser: null,
-            searchQuery: ''
+            searchQuery: '',
+            showUsersWithNoAccounts: false
         };
     },
     mounted() {
@@ -131,6 +46,10 @@ export default {
     },
     computed: {
         filteredUsers() {
+            if (this.showUsersWithNoAccounts) {
+                return this.users.filter(user => !user.bankAccountList || user.bankAccountList.length === 0);
+            }
+
             if (this.searchQuery) {
                 const query = this.searchQuery.toLowerCase();
                 return this.users.filter(user => {
@@ -138,6 +57,7 @@ export default {
                     return fullName.includes(query);
                 });
             }
+
             return this.users;
         }
     },
@@ -159,13 +79,38 @@ export default {
         },
         selectUser(user) {
             this.$router.push(`/employee/accounts/` + btoa(user.id));
+        },
+        filterUsersWithNoAccounts() {
+            this.showUsersWithNoAccounts = true;
+        },
+        resetFilter() {
+            this.showUsersWithNoAccounts = false;
         }
     }
 };
 </script>
+
+
   
 <style>
-@import '../../assets/css/allAccounts.css';
->>>>>>> e9e4c7c0bf8e179c2f11f9e88ef20c53cb951af8
+#inputAndBtn {
+    display: flex;
+    justify-content: space-between;
+    color: none;
+}
+
+#buttonAdd {
+    height: 40px;
+    margin-bottom: 10px;
+}
+
+#inputfield {
+    height: 40px;
+}
+
+
+#buttonFilter {
+    height: 40px;
+    margin-left: 10px;
+}
 </style>
-  
