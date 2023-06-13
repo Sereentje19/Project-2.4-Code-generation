206af5af41e66e2d9367e3cf4126940e05f4f54b
Serena <Serena.kenter@gmail.com>
2023-06-13 19:59:56 +0200

    Update BankAccountStepDefinitions.java

--- Back-end/code-generation/src/test/java/SOT/Squad/code/generation/Cucumber/BankAccountStepDefinitions.java
+++ Back-end/code-generation/src/test/java/SOT/Squad/code/generation/Cucumber/BankAccountStepDefinitions.java
@@ -4,7 +4,6 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-
 public class BankAccountStepDefinitions extends baseStepDefinitions{
     
     @Given("a bank account with the following details:")
