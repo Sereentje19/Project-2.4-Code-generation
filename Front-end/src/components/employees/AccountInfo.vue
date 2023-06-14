e47c2c4c346b1d16dc14bb475d9385cbc2bef30d
647825 <89908526+647825@users.noreply.github.com>
2023-06-14 20:05:24 +0200

    last updates

--- Front-end/src/components/employees/AccountInfo.vue
+++ Front-end/src/components/employees/AccountInfo.vue
@@ -108,6 +108,9 @@ export default {
     const decodedId = atob(encodedId);
     this.getUserInfo(decodedId);
   },
+  created() {
+    this.editedUser = { ...this.customer };
+  },
   methods: {
     getUserInfo(userId) {
       axios
@@ -118,6 +121,7 @@ export default {
         })
         .then((res) => {
           this.customer = res.data;
+          this.editedUser = { ...this.customer };
           this.getBankAccounts();
         })
         .catch(error => console.log(error));
@@ -128,13 +132,16 @@ export default {
       console.log(this.customer.id);
       console.log(this.editedUser.id);
       console.log(this.headerToken);
+      if (!this.checkFieldsNotEmpty()) {
+        return;
+      }
       axios
         .put('/users/' + this.customer.id, this.editedUser, headerToken)
         .then((response) => {
           console.log('User information updated successfully!');
-          this.customer = { ...this.editedUser };
+          this.customer = { ...this.editedUser }; // Update customer with editedUser data
           this.editMode = false;
-          console.log(this.response)
+          console.log(response);
         })
         .catch((error) => {
           console.error('Error updating user information:', error);
@@ -142,9 +149,25 @@ export default {
     },
     cancelEdit() {
       this.editMode = false;
+      this.editedUser = { ...this.customer }; // Reset editedUser to customer data
     },
     goBack() {
       this.$router.go(-1); // Go back to the previous page
+    },
+    checkFieldsNotEmpty() {
+      if (!this.customer.email.includes('@')) {
+        alert('Please enter a valid email address.');
+        return false;
+      } else if (!/^\d{10,}$/.test(this.user.phoneNumber)) {
+        alert("Phonenumber has to be at least 10 numbers");
+        return false;
+      } else if (!this.user.firstName || !this.user.lastName
+        || !this.user.postalCode || !this.user.city
+        || !this.user.street || !/^\d+$/.test(this.user.houseNumber)) {
+        alert("Please fill al fields before savings all changes");
+        return false;
+      }
+      return true;
     }
   }
 };
