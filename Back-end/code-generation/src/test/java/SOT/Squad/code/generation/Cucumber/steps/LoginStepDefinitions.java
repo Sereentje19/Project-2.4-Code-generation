25518836b61551bd41528051e1e9f02748f88ae6
Serena <Serena.kenter@gmail.com>
2023-06-14 20:41:44 +0200

    cmdecde

--- Back-end/code-generation/src/test/java/SOT/Squad/code/generation/Cucumber/steps/LoginStepDefinitions.java
+++ Back-end/code-generation/src/test/java/SOT/Squad/code/generation/Cucumber/steps/LoginStepDefinitions.java
@@ -8,12 +8,11 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import java.util.List;
 
 public class LoginStepDefinitions {
 
@@ -38,29 +37,81 @@ public class LoginStepDefinitions {
         httpHeaders.add("Authorization", "Bearer " + jwtToken);
     }
 
-
-    @When("I submit my login credentials")
-    public void iSubmitMyLoginCredentials() {
+    @When("I submit my login credentials username {string} and password {string}")
+    public void iSubmitMyLoginCredentialsUsernameAndPassword(String username, String password) {
+        iHaveValidLoginCredentialsUsernameAndPassword(username, password);
     }
 
     @Then("the response should be a successful login response with a token")
     public void theResponseShouldBeASuccessfulLoginResponseWithAToken() {
+        assertNotNull(responseEntity);
+
+        // Print the response status code and body for debugging purposes
+        System.out.println("Response Status Code: " + responseEntity.getStatusCodeValue());
+        System.out.println("Response Body: " + responseEntity.getBody());
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(responseEntity.getBody());
+
+        // Assuming the response body is a JSON object containing a "token" field
+        String token = JsonPath.read(responseEntity.getBody(), "$.token");
+        assertNotNull(token);
     }
 
     @Given("I have invalid login credentials username {string} and password {string}")
     public void iHaveInvalidLoginCredentialsUsernameAndPassword(String username, String password) {
+        httpHeaders.add("Content-Type", "application/json");
+
+        // Create a request with invalid credentials
+        String invalidCredentials = "{\"username\":\"" + username + "\", \"password\":\"" + password + "\"}";
+        responseEntity = restTemplate.exchange(
+                uri + "login",
+                HttpMethod.POST,
+                new HttpEntity<>(invalidCredentials, httpHeaders),
+                String.class
+        );
     }
 
     @Then("the response should be an error indicating invalid credentials")
     public void theResponseShouldBeAnErrorIndicatingInvalidCredentials() {
+        assertNotNull(responseEntity);
+
+        String response = responseEntity.getBody();
+
+        // Assuming the error message is located at the root level of the response body
+//        String errorMessage = JsonPath.read(response, "$.message");
+
+        assertNotNull(response);
+        assertTrue(response.contains("invalid credentials"));
     }
 
     @Given("I have missing login credentials")
     public void iHaveMissingLoginCredentials() {
+        // No action needed, since we are intentionally submitting without credentials
+    }
+
+    @When("I submit without filling in my login credentials")
+    public void iSubmitWithoutFillingInMyLoginCredentials() {
+        httpHeaders.add("Content-Type", "application/json");
+
+        // Create a request without any credentials
+        responseEntity = restTemplate.exchange(
+                uri + "login",
+                HttpMethod.POST,
+                new HttpEntity<>(httpHeaders),
+                String.class
+        );
     }
 
     @Then("the response should be an error indicating missing credentials")
     public void theResponseShouldBeAnErrorIndicatingMissingCredentials() {
+        assertNotNull(responseEntity);
+
+        // Assuming the error message is located at the root level of the response body
+        String errorMessage = JsonPath.read(responseEntity.getBody(), "$.message");
+
+        assertNotNull(errorMessage);
+        assertTrue(errorMessage.toLowerCase().contains("missing credentials"));
     }
 
 }
